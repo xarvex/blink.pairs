a16eb1a56500509b995c7483a8546a3d6e7ef090
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-04-27 06:50:59 -0500

    fix(build): create target directory if not exist

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
     // delete existing version file created by blink.download
     let _ = std::fs::remove_file("target/release/version");
+
+    std::fs::create_dir_all("target/release").unwrap();
 }
